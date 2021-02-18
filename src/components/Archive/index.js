6b49ba78cab502a54520c0eb13118b30c393723b
brownspunbear <benjaminrshill@hotmail.com>
2021-02-18 12:21:27 +0000

    archive updated ok

--- src/components/Archive/index.js
+++ src/components/Archive/index.js
@@ -13,27 +13,11 @@ export default function Archive(props) {
         } else return [];
     }
 
-    function doArchiveUpdate() {
-        let newA = [...archive];
-        newA.forEach(week => {
-            week.items.forEach(it => {
-                if (!it.type) {
-                    it.todo.forEach((td, i) => {
-                        if (td === 2) it.todo[i] = 20;
-                        if (td === 3) it.todo[i] = 30;
-                    });
-                }
-            });
-        });
-        localStorage.setItem('archive', JSON.stringify(newA));
-    }
-
     return (
         <main id='arrange'>
             <h1>
                 Archive
             </h1>
-            <button onClick={doArchiveUpdate} style={{background: 'blue'}}>UPDATE</button>
             {archive.map((week, i) =>
                 <ArchiveWeek
                     key={'archiveWeek' + i}
