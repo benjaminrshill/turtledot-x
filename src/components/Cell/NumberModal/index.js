cebb6e450fe0bf5d828138753958198a8c7760bc
brownspunbear <benjaminrshill@hotmail.com>
2021-05-03 12:56:15 +0100

    remove timer, keep some modifications

--- src/components/Cell/NumberModal/index.js
+++ src/components/Cell/NumberModal/index.js
@@ -1,66 +1,12 @@
 import React, {useState} from 'react';
 import cutNumber from '../../../functions/cutNumber';
-import {Check, Watch} from 'react-feather';
+import {Check} from 'react-feather';
 import './numberModal.css';
-import './Timer/timer.css'
-
-// class Timer {
-//     interval = 1000;
-//     expected;
-//     timeout;
-//
-//     getStart() {
-//         this.start(this.step, this.interval);
-//     }
-//
-//     start(step, interval) {
-//         this.expected = Date.now() + interval;
-//         this.timeout = setTimeout(() => step(step, interval), interval);
-//     }
-//
-//     stop() {
-//         clearTimeout(this.timeout);
-//     }
-//
-//     step(step, interval) {
-//         let drift = Date.now() - this.expected;
-//         if (drift > this.interval) {
-//             console.warn('drift is too big');
-//         }
-//         this.doTime();
-//         this.expected += this.interval;
-//         this.timeout = setTimeout(this.step, Math.max(0, interval - drift));
-//     }
-//
-//     doTime() {
-//         console.log(this.expected);
-//     }
-// }
-
-class Timer {
-    interval = 1000;
-    doZero(interval) {
-        return interval < 10 ? '0'+interval : interval;
-    };
-    Date = Date.now() + this.interval;
-    Hour = this.Date.getHours();
-    Mins = this.doZero(this.Date.getMinutes());
-    Secs = this.doZero(this.Date.getSeconds());
-}
-
-setInterval(() => {
-    let timer = new Timer();
-    document.querySelector('.hourMin').textContent = timer.Hour + ':' + timer.Mins;
-    document.querySelector('.sec').textContent = timer.Secs;
-}, 1000);
 
 export default function NumberModal(props) {
 
     const [currentValue, editCurrentValue] = useState(props.day);
     const [inputNumber, editInputNumber] = useState(props.day > 0 ? props.day : cutNumber(props.avgTodo));
-    const [timerOpen, switchTimerOpen] = useState(false);
-
-    const timer = new Timer();
 
     function validateTodo() {
         if (currentValue > -2 && currentValue < 100001) {
@@ -142,37 +88,6 @@ export default function NumberModal(props) {
                             className='save-todo'
                         >
                             <Check size={16} />
-                        </button>
-                    </div>
-                    <div>
-                        <button
-                            onClick={() => switchTimerOpen(true)}
-                            className='open-timer'
-                        >
-                            <Watch size={16} />
-                        </button>
-                    </div>
-                </div>
-                <div
-                    onClick={() => switchTimerOpen(false)}
-                    className={'timer-bg modal-bg' + (timerOpen ? ' open' : '')}
-                >
-                </div>
-                <div className={'timer' + (timerOpen ? ' open' : '')}>
-                    <div className='time'>
-                    </div>
-                    <div className='actions-panel'>
-                        <button
-                            onClick={timer.getStart}
-                            className='timer-start'
-                        >
-                            start
-                        </button>
-                        <button
-                            onClick={timer.stop}
-                            className='timer-stop'
-                        >
-                            stop
                         </button>
                     </div>
                 </div>
